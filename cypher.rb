41e5b5321aea9ce1550a97d98fbf8be4a74af90c
Ashley <a.clarkem@gmail.com>
2014-10-22 10:43:07 -0600

    cards can be moved in deck

--- cypher.rb
+++ cypher.rb
@@ -1,3 +1,5 @@
+require 'pp'
+
 class MyString
   def format(string)
     string.gsub!(/\W/, '')
@@ -10,9 +12,36 @@ class MyString
         new_string.concat(s + " ")
       end
     end
-  new_string.upcase!
+    new_string.upcase!
   end
 end
 
+
+class Deck
+  def initialize
+    @deck = [*1..52, 'A', 'B']
+  end
+
+  def cards
+    p @deck
+  end
+
+
+  def move(card, spaces)
+    index = @deck.index(card)
+    @deck.delete_at(index)
+    if index + spaces <= 53
+      @deck.insert((index + spaces), card)
+    else
+      @deck.insert((spaces - (54-index)), card)
+    end
+    p @deck
+  end
+end
+
+
+Deck.new.move("A", 0)
+
+
 string = "Now this is a story all about how..."
-p MyString.new.format(string)
+MyString.new.format(string)
